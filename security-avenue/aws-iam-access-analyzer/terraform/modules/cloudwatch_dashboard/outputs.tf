c359ca620e4a6c35aa4fc28b45d47af05570cb9f
ambekadeshmukh <ambeka@live.com>
2025-05-10 14:31:50 -0400

    folders removed

--- security-avenue/aws-iam-access-analyzer/terraform/modules/cloudwatch_dashboard/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "dashboard_arn" {
-  description = "ARN of the CloudWatch Dashboard"
-  value       = aws_cloudwatch_dashboard.access_analyzer_dashboard.dashboard_arn
-}
-
-output "dashboard_name" {
-  description = "Name of the CloudWatch Dashboard"
-  value       = aws_cloudwatch_dashboard.access_analyzer_dashboard.dashboard_name
-}
--- security-avenue/aws-iam-access-analyzer/terraform/modules/config_rules/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "config_rule_arns" {
-  description = "ARNs of the created AWS Config Rules"
-  value = [
-    aws_config_config_rule.s3_bucket_public_access.arn,
-    aws_config_config_rule.iam_root_access_key.arn,
-    aws_config_config_rule.iam_user_mfa.arn
-  ]
-}
